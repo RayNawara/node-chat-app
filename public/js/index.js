17708d13dfb007adadb185532544cc9b4554c831
Ray Nawara <rjnawara@gmail.com>
2018-10-25 16:28:11 -0400

    Add form for messages and show incoming messages in browser

--- public/js/index.js
+++ public/js/index.js
@@ -2,18 +2,27 @@ let socket = io();
 
 socket.on('connect', function () {
   console.log('Connected to server');
-
-  // socket.emit('createMessage', {
-  //   from: 'Ray',
-  //   text: 'Hi! Sure, 6:30 is OK by me.'
-  // });
 });
 
 socket.on('newMessage', function (message) {
   console.log('Got a new message', message);
+  let li = jQuery('<li></li>');
+  li.text(`${message.from}: ${message.text}`);
+
+  jQuery('#messages').append(li);
+});
 
 socket.on('disconnect', function () {
   console.log('Disconnected from server');
 });
 
+jQuery('#message-form').on('submit', function (e) {
+  e.preventDefault();
+
+  socket.emit('createMessage', {
+    from: 'User',
+    text: jQuery('[name=message]').val()
+  }, function () {
+
+  });
 });
